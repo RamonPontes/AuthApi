808a777b98e7e8b3cefae65c590c71cb121d150f
RamonPontes <rgdpontes@yahoo.com>
2024-12-08 22:58:12 -0300

    Add find user with Email

--- src/main/java/com/ramon/authApi/repository/AuthRepository.java
+++ src/main/java/com/ramon/authApi/repository/AuthRepository.java
@@ -3,4 +3,6 @@ package com.ramon.authApi.repository;
 import com.ramon.authApi.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AuthRepository extends JpaRepository<User, Integer> {}
+public interface AuthRepository extends JpaRepository<User, Integer> {
+    User findByEmail(String email);
+}
--- src/main/java/com/ramon/authApi/service/AuthService.java
+++ src/main/java/com/ramon/authApi/service/AuthService.java
@@ -1,4 +1,16 @@
 package com.ramon.authApi.service;
 
+import com.ramon.authApi.model.User;
+import com.ramon.authApi.repository.AuthRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class AuthService {
+
+    @Autowired
+    AuthRepository authRepository;
+
+    public User findUserWithEmail(String email) {
+        return authRepository.findByEmail(email);
+    }
+
 }
