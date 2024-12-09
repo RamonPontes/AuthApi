744e8584cc9ee637c81a9767499dc585cb34bcc6
RamonPontes <rgdpontes@yahoo.com>
2024-12-08 23:33:40 -0300

    Add password encoder

--- src/main/java/com/ramon/authApi/AuthApiApplication.java
+++ src/main/java/com/ramon/authApi/AuthApiApplication.java
@@ -2,8 +2,9 @@ package com.ramon.authApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class AuthApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/ramon/authApi/service/AuthService.java
+++ src/main/java/com/ramon/authApi/service/AuthService.java
@@ -4,12 +4,15 @@ import com.ramon.authApi.dto.LoginRequest;
 import com.ramon.authApi.model.User;
 import com.ramon.authApi.repository.AuthRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
 
 @Service
 public class AuthService {
+
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     @Autowired
     AuthRepository authRepository;
@@ -19,13 +22,15 @@ public class AuthService {
     }
 
     public User registredUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
         return authRepository.save(user);
     }
 
     public User login(LoginRequest userData) {
         User user = authRepository.findByEmail(userData.getEmail());
 
-        if (Objects.equals(user.getPassword(), userData.getPassword())) {
+        if (passwordEncoder.matches(userData.getPassword(), user.getPassword())) {
             return user;
         } else {
             return null;
